2459a1c47f65fb863006b4a0de77877a02847f53
Rafael Martins <rafaelmgomes.dev@gmail.com>
2022-11-24 12:02:19 -0300

    Lapidando parte de recuperação

--- Config/contract.hpp
+++ Config/contract.hpp
@@ -1,27 +1,23 @@
 #ifndef __CONTRACT_H__
 #define __CONTRACT_H__
 
-#include <list>
+#include <iostream>
 #include <string>
 #include <map>
 #include <set>
 #include <vector>
-#include <iterator>
-#include <algorithm>
 #include <fstream>
 #include <filesystem>
+
 using namespace std;
 
 namespace fs = filesystem;
-
-// using recursive_directory_iterator = std::filesystem::recursive_directory_iterator;
-
 
 class Indice {
     public:
         Indice();
         void imprime();
-        
+        set<string> recuperacao(string frase);
     private:
         map<string, set<string>> elementos_;
 };
